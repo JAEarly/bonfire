5518a2057e1822e6087b86e36f46da5119a5b0c2
Joseph Early <je5g15@soton.ac.uk>
2022-03-17 19:19:03 +0000

    Updated musk dataset.

--- pytorch_mil/data/musk_dataset.py
+++ pytorch_mil/data/musk_dataset.py
@@ -16,9 +16,32 @@ MUSK_D_IN = 166
 class MuskDataset(MilDataset):
 
     def __init__(self, bag_names, bags, targets):
-        super().__init__(bags, targets, None)
+        super().__init__("Musk", bags, targets, None)
         self.bag_names = bag_names
 
+    @classmethod
+    def create_datasets(cls, musk_two=False, random_state=12):
+        parsed_data = parse_data(musk_two)
+        bag_names, bags, targets = parsed_data
+
+        bags = normalise(bags)
+
+        splits = train_test_split(bag_names, bags, targets, train_size=0.7, stratify=targets, random_state=random_state)
+
+        train_bag_names, train_bags, train_targets = [splits[i] for i in [0, 2, 4]]
+        test_bag_names, test_bags, test_targets = [splits[i] for i in [1, 3, 5]]
+
+        splits = train_test_split(test_bag_names, test_bags, test_targets, train_size=0.5, stratify=test_targets,
+                                  random_state=random_state)
+
+        val_bag_names, val_bags, val_targets = [splits[i] for i in [0, 2, 4]]
+        test_bag_names, test_bags, test_targets = [splits[i] for i in [1, 3, 5]]
+
+        train_dataset = MuskDataset(train_bag_names, train_bags, train_targets)
+        val_dataset = MuskDataset(val_bag_names, val_bags, val_targets)
+        test_dataset = MuskDataset(test_bag_names, test_bags, test_targets)
+
+        return train_dataset, val_dataset, test_dataset
 
 def parse_data(musk_two):
     path = MUSK2_FILE_PATH if musk_two else MUSK1_FILE_PATH
@@ -66,32 +89,3 @@ def normalise(bags):
         norm_bag = (bag - dataset_mean) / dataset_std
         norm_bags.append(norm_bag)
     return norm_bags
-
-
-def create_datasets(musk_two=False, random_state=12):
-    parsed_data = parse_data(musk_two)
-    bag_names, bags, targets = parsed_data
-
-    bags = normalise(bags)
-
-    splits = train_test_split(bag_names, bags, targets, train_size=0.7, stratify=targets, random_state=random_state)
-
-    train_bag_names, train_bags, train_targets = [splits[i] for i in [0, 2, 4]]
-    test_bag_names, test_bags, test_targets = [splits[i] for i in [1, 3, 5]]
-
-    splits = train_test_split(test_bag_names, test_bags, test_targets, train_size=0.5, stratify=test_targets,
-                              random_state=random_state)
-
-    val_bag_names, val_bags, val_targets = [splits[i] for i in [0, 2, 4]]
-    test_bag_names, test_bags, test_targets = [splits[i] for i in [1, 3, 5]]
-
-    train_dataset = MuskDataset(train_bag_names, train_bags, train_targets)
-    val_dataset = MuskDataset(val_bag_names, val_bags, val_targets)
-    test_dataset = MuskDataset(test_bag_names, test_bags, test_targets)
-
-    return train_dataset, val_dataset, test_dataset
-
-
-if __name__ == "__main__":
-    for d in create_datasets(musk_two=False):
-        d.summarise()
