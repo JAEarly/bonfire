2a8658cc2fa03c79b4d4b0b0fc3918803b4f8dca
Joseph Early <je5g15@soton.ac.uk>
2022-03-17 20:20:26 +0000

    Removed dropout from crc model conv block and added crc lstm model.

--- pytorch_mil/model/benchmark/crc_models.py
+++ pytorch_mil/model/benchmark/crc_models.py
@@ -11,8 +11,8 @@ class CrcEncoder(nn.Module):
 
     def __init__(self, ds_enc_hid, d_enc, dropout):
         super().__init__()
-        conv1 = mod.ConvBlock(c_in=3, c_out=36, kernel_size=4, stride=1, padding=0, dropout=dropout)
-        conv2 = mod.ConvBlock(c_in=36, c_out=48, kernel_size=3, stride=1, padding=0, dropout=dropout)
+        conv1 = mod.ConvBlock(c_in=3, c_out=36, kernel_size=4, stride=1, padding=0)
+        conv2 = mod.ConvBlock(c_in=36, c_out=48, kernel_size=3, stride=1, padding=0)
         self.fe = nn.Sequential(conv1, conv2)
         self.fc_stack = mod.FullyConnectedStack(CRC_FV_SIZE, ds_enc_hid, d_enc, dropout, raw_last=False)
 
@@ -82,4 +82,21 @@ class CrcGNN(models.ClusterGNN):
         return {
             'lr': 1e-3,
             'weight_decay': 1e-2,
+        }
+
+
+class CrcMiLSTM(models.AttentionNN):
+
+    def __init__(self, device, n_classes, d_enc=256, ds_enc_hid=(64, 64), d_lstm_hid=256, n_lstm_layers=1,
+                 bidirectional=False, ds_fc_hid=(), dropout=0.2):
+        encoder = CrcEncoder(ds_enc_hid, d_enc, dropout)
+        aggregator = agg.LstmAggregator(d_enc, d_lstm_hid, n_lstm_layers, bidirectional, dropout, ds_fc_hid,
+                                        n_classes)
+        super().__init__(device, n_classes, CRC_N_EXPECTED_DIMS, encoder, aggregator)
+
+    @overrides
+    def suggest_train_params(self):
+        return {
+            'lr': 1e-4,
+            'weight_decay': 1e-6,
         }
