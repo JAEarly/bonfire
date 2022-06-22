e79bc1ec2274eaf64e9fc18ec758c03f73facb49
Joseph Early <je5g15@soton.ac.uk>
2022-06-22 10:40:59 +0100

    Fixed default model save path.

--- bonfire/train/__init__.py
+++ bonfire/train/__init__.py
@@ -4,7 +4,7 @@ DEFAULT_SEEDS = [868, 207, 702, 999, 119, 401, 74, 9, 741, 744]
 
 def get_default_save_path(dataset_name, model_name, modifier=None):
     # Save directory
-    save_dir = "models/{:s}/{:s}".format(dataset_name, dataset_name, model_name)
+    save_dir = "models/{:s}/{:s}".format(dataset_name, model_name)
 
     # File name with optional additions
     file_name = "{:s}".format(model_name)
